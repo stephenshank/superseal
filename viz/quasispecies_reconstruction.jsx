6a872f4ece1469b125465d29ed1a2d587fad5fef
Stephen Shank <sshank314@gmail.com>
2019-07-22 11:54:23 -0400

    Quasispecies frequency alignment viewer/bar chart.

--- viz/quasispecies_reconstruction.jsx
+++ viz/quasispecies_reconstruction.jsx
@@ -1,15 +1,44 @@
 import React, { Component } from "react";
+import { SequenceBarChart, fastaParser } from "alignment.js";
+import axios from "axios";
+
+
+function QuasispeciesAlignment(props) {
+  if (!props.fasta) return null;
+  const sequence_data = fastaParser(props.fasta),
+    data = sequence_data.map(
+      record => +record.header.split('_')[1].split('-')[1],
+    );
+  sequence_data.forEach(record => {
+    record.header = record.header.split('_')[0]
+  });
+  return (<SequenceBarChart
+    sequence_data={sequence_data}
+    data={data}
+    label="Quasispecies Frequency"
+  />);
+}
 
 class QuasispeciesReconstruction extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      error: null
+      error: null,
+      fasta: null
     }
+  }
+  componentDidMount() {
+    axios.get('/api/quasispecies.fasta')
+      .then(response => {
+        this.setState({fasta: response.data})
+      }).catch(error => {
+        this.setState({error:true});
+      });
   }
   render() {
     return (<div>
       <h1>Quasispecies Reconstruction</h1>
+      <QuasispeciesAlignment fasta={this.state.fasta} />
     </div>);
   }
 }
